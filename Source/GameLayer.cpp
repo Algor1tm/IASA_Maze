6bf5b542d9f6aa20272316a946e96670c87ef74b
OstapIasaProg <148550575+OstapIasaProg@users.noreply.github.com>
2023-12-10 11:18:06 +0200

    Add files via upload

---
+++ Source/GameLayer.cpp
@@ -0,0 +1,20 @@
+#include "GameLayer.h"
+
+#include <iostream>
+
+
+GameLayer::GameLayer()
+{
+	m_Maze = new Maze();
+}
+
+GameLayer::~GameLayer()
+{
+	delete m_Maze;
+}
+
+void GameLayer::OnUpdate()
+{
+	m_Maze->Step();
+	std::cout << "GameLayer::OnUpdate()\n\n";
+}
